2d5c8037d153b29141071649cdb6d10819f863ba
torkleyy <torkleyy@gmail.com>
2017-12-09 15:34:16 +0100

    Allow using references as argument

---
+++ examples/ref.rs
@@ -0,0 +1,52 @@
+extern crate machina;
+
+use machina::*;
+
+enum HelloState {
+    Hello,
+    Bye,
+}
+
+impl<'a> State<&'a str, (), ()> for HelloState {
+    fn start(&mut self, args: &str) -> Result<Trans<Self>, ()> {
+        match *self {
+            HelloState::Hello => {
+                println!("Hey, {}", args);
+            }
+            HelloState::Bye => {
+                println!("Bye, {}", args);
+            }
+        }
+
+        Ok(Trans::None)
+    }
+
+    fn update(&mut self, args: &str) -> Result<Trans<Self>, ()> {
+        match *self {
+            HelloState::Hello => {
+                println!("Update: {}", args);
+
+                Ok(Trans::Push(HelloState::Bye))
+            }
+            HelloState::Bye => Ok(Trans::Quit),
+        }
+    }
+}
+
+fn run() -> Result<(), ()> {
+    let mut machine = StateMachineRef::new(HelloState::Hello);
+
+    machine.start("you!")?;
+    machine.update("Whatever")?;
+    machine.update("Irrelevant")?;
+
+    assert!(!machine.running());
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("Error occurred: {:?}", e);
+    }
+}
